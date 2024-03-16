56c005b892a14f5aa7759c97c7465c0896796820
Augusto Silva <augusto.a.silva@hotmail.com>
2024-03-16 10:43:44 -0300

    feat: adiciona duas novas exceptions to order controller advisor

--- src/main/java/com/fiap/fourlanches/order/adapter/driver/api/controllersAdvisor/OrderControllerAdvisor.java
+++ src/main/java/com/fiap/fourlanches/order/adapter/driver/api/controllersAdvisor/OrderControllerAdvisor.java
@@ -1,9 +1,11 @@
 package com.fiap.fourlanches.order.adapter.driver.api.controllersAdvisor;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fiap.fourlanches.order.application.exception.FailPublishToQueueException;
 import com.fiap.fourlanches.order.application.exception.IncorrectOrderStatusException;
 import com.fiap.fourlanches.order.domain.exception.InvalidOrderException;
 import com.fiap.fourlanches.order.domain.exception.OrderNotFoundException;
+import org.springframework.amqp.AmqpException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -36,6 +38,20 @@ public class OrderControllerAdvisor extends GeneralControllerAdvisor {
     @ExceptionHandler(FailPublishToQueueException.class)
     public ResponseEntity<ApiErrorMessage> handleFailPublishToQueueException() {
         var errorMessage = new ApiErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, "Fail to publish message to queue");
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(JsonProcessingException.class)
+    public ResponseEntity<ApiErrorMessage> handleJsonProcessingException() {
+        var errorMessage = new ApiErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, "Error generating message to send to queue");
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(AmqpException.class)
+    public ResponseEntity<ApiErrorMessage> handleAmqpException() {
+        var errorMessage = new ApiErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, "An error happened while sending message to queue");
 
         return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
     }
